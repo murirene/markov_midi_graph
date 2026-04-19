748c236088c6e8cf767dc089e6986909d769d8bd
Rene Murillo <rene.murillo.texas@gmail.com>
2026-04-19 02:01:55 -0700

    playing with color

--- markov_midi_graph/src/MusicComposition.tsx
+++ markov_midi_graph/src/MusicComposition.tsx
@@ -1,9 +1,24 @@
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
+import {timer} from "d3"
 
 type MusicCompositionProps = { fill:string }
+type MusicPoint = { x: number, y: number }
 
 const MusicComposition = ({fill}: MusicCompositionProps) => {
-  return (<circle cx="400" cy="400" r="100" fill={`hsl(${fill}, 100%, 50%)`} />)
+  const [pos, setPos] = useState<MusicPoint>({x: 400, y: 300})
+
+  useEffect(() => {
+    const animation = timer((elapsed) => {
+      const x = 400 + Math.cos(elapsed / 700) * 120
+      const y = 300 + Math.sin(elapsed / 900) * 90
+
+      setPos({x,y})
+    })
+
+    return () => animation.stop()
+  }, [])
+
+  return (<circle cx={pos.x} cy={pos.y} r="100" fill={`hsl(${fill}, 100%, 50%)`} />)
 }
 
 
